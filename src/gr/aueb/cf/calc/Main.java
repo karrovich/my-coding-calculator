845b197aa743ccdf681f284fbac88f3ff35c6911
V. Karras <v.karras98@gmail.com>
2025-01-29 20:02:13 +0200

    Hotfix. try catch added for handling arithmetic exception

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -29,6 +29,11 @@ public class Main {
     }
 
     public static int mod(int a, int b) {
-        return a % b;
+        try {
+            return a % b;
+        } catch (ArithmeticException e) {
+            System.err.println("Error. Denominator cannot be zero");
+            throw e;
+        }
     }
 }
